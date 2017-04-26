6e3a04c1ae6973a9c5778c497bc869330e798370
yoonchulgyu <dbscjfrb1@naver.com>
2017-04-27 03:42:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdT0x4UAAAAAHyTXX1f06aaSvE6LWBJ4K1I66YR',  // <----- EDIT ME!
-  redirect_uri: 'https://github.com/yoonchulgyu/dicemp.github.io',
+  redirect_uri: 'https://dicemp.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
